4b0b2d013e7eeb97162fd89b88f60754bd56e9fd
Zack Cerza <zack@redhat.com>
2016-04-19 16:14:42 -0600

    add basic html render

--- teuthology/task/pcp.py
+++ teuthology/task/pcp.py
@@ -4,6 +4,7 @@
 # record time range + hosts
 # locate pcp archive
 # generate graphs
+import jinja2
 import logging
 import os
 import requests
@@ -25,7 +26,7 @@ class Grapher(object):
         format='png',
     )
 
-    def __init__(self, remotes, time_from, time_until):
+    def __init__(self, remotes, time_from, time_until='now'):
         self.remotes = remotes
         self.time_from = time_from
         self.time_until = time_until
@@ -54,6 +55,7 @@ class PCP(Task):
     metrics = [
         'kernel.all.load.1 minute',
         'mem.util.free',
+        'mem.util.used',
         'network.interface.*.bytes.*',
         'disk.all.read_bytes',
         'disk.all.write_bytes',
@@ -62,22 +64,35 @@ class PCP(Task):
     def __init__(self, ctx, config):
         super(PCP, self).__init__(ctx, config)
         self.log = log
+        # until the job stops, we may want to render graphs reflecting the most
+        # current data
+        self.stop_time = 'now'
 
     def setup(self):
         super(PCP, self).setup()
+        if not self.ctx.archive:
+            return
+        self.out_dir = os.path.join(
+            self.ctx.archive,
+            'pcp_graphs',
+        )
+        os.mkdir(self.out_dir)
 
     def begin(self):
         self.start_time = int(time.time())
         log.debug("cluster: %s", self.cluster)
         log.debug("start_time: %s", self.start_time)
+        self.build_graph_urls()
+        self.write_html()
 
     def end(self):
         self.stop_time = int(time.time())
         log.debug("stop_time: %s", self.start_time)
-        self.get_graph_urls()
+        self.build_graph_urls()
         self.download_graphs()
+        self.write_html(mode='static')
 
-    def get_graph_urls(self):
+    def build_graph_urls(self):
         self.grapher = Grapher(
             remotes=self.cluster.remotes.keys(),
             time_from=self.start_time,
@@ -92,16 +107,11 @@ class PCP(Task):
     def download_graphs(self):
         if not self.ctx.archive:
             return
-        graphs_dir = os.path.join(
-            self.ctx.archive,
-            'pcp_graphs',
-        )
-        os.mkdir(graphs_dir)
         for metric in self.graphs.keys():
             url = self.graphs[metric]['url']
             filename = self._sanitize_metric_name(metric) + '.png'
             self.graphs[metric]['file'] = graph_path = os.path.join(
-                graphs_dir,
+                self.out_dir,
                 filename,
             )
             resp = requests.get(url)
@@ -126,6 +136,27 @@ class PCP(Task):
         for rep in replacements:
             result = result.replace(rep[0], rep[1])
         return result
+
+    def write_html(self, mode='dynamic'):
+        if not self.ctx.archive:
+            return
+        generated_html = self.generate_html(mode=mode)
+        html_path = os.path.join(self.out_dir, 'pcp.html')
+        with open(html_path, 'w') as f:
+            f.write(generated_html)
+
+    def generate_html(self, mode='dynamic'):
+        cwd = os.path.dirname(__file__)
+        loader = jinja2.loaders.FileSystemLoader(cwd)
+        env = jinja2.Environment(loader=loader)
+        template = env.get_template('pcp.j2')
+        log.debug(str(self.ctx.config))
+        data = template.render(
+            job_id=self.ctx.config.get('job_id'),
+            graphs=self.graphs,
+            mode=mode,
+        )
+        return data
 
 
 task = PCP
